993268953ed00ebde61eb54a6a2679b861d23875
Chirag Jain <chirag8966@gmail.com>
2025-04-17 20:29:26 +0530

    feat: add tailwind 3 & theme switcher

---
+++ front-end/src/app/common/theme-switcher/theme-switcher.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-switcher',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './theme-switcher.component.html',
+  styleUrl: './theme-switcher.component.scss'
+})
+export class ThemeSwitcherComponent {
+  isDarkMode = false;
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
+}
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import PrimeUI from 'tailwindcss-primeui';
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+  darkMode: ["variant", ".dark &"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [PrimeUI]
 }
 
