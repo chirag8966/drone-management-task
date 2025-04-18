e37f940d98b99888eea0e1ead9d44da281bd5d35
Chirag Jain <chirag8966@gmail.com>
2025-04-18 20:10:54 +0530

    feat: google maps integrated

--- front-end/src/app/pages/planning/missions/missions.component.ts
+++ front-end/src/app/pages/planning/missions/missions.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { GoogleMapComponent } from './google-map/google-map.component';
 
 @Component({
   selector: 'app-missions',
-  imports: [],
+  standalone: true,
+  imports: [GoogleMapComponent],
   templateUrl: './missions.component.html',
-  styleUrl: './missions.component.scss'
+  styleUrls: ['./missions.component.scss']
 })
 export class MissionsComponent {
-
+  center = { lat: 18.566613521086374, lng: 73.77209687152218 };
+  markers = [
+    { lat: 18.566613521086374, lng: 73.77209687152218 }
+  ];
 }
